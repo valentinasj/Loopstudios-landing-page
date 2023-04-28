bb2ea7b027cf19a984d5db558c1b839b98e309c4
valestina jimenez <valentinajimesala@gmail.com>
2023-04-28 16:08:09 -0500

    carpeta App actualizada

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom/client';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
   </React.StrictMode>
 );
 
