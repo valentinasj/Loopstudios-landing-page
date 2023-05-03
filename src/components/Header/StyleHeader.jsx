6cf3efd395f8d94af46760f19839598bdd0de33b
valestina jimenez <valentinajimesala@gmail.com>
2023-05-03 12:16:47 -0500

    header listo

--- src/components/Header/StyleHeader.jsx
+++ src/components/Header/StyleHeader.jsx
@@ -2,34 +2,39 @@ import styled from "styled-components";
 import backgroundImage from "../../images/desktop/image-hero.jpg";
  
 export const StyleContainer = styled.div`
- padding: 0;
+ padding: 90px;
  height: 100vh;
+ width: 100vw ;
  background-image: url("${backgroundImage}");
 `;
 
 export const StyleHeader = styled.div`
-  
   display: flex;
   justify-content: space-between;
-  align-items: center;
   background-position: center;
   background-size: cover;
   ul {
-    padding: 20px;
+    font-family: 'Alata', sans-serif;
+    color: #fff;
     list-style: none;
     display: flex;
     gap: 15px;
-    align-items: center;
   }
 `;
 
 export const StyleText = styled.section`
+  padding: 50px;
+  font-family: 'Josefin Sans', sans-serif;
   color: white;
   border: 1px solid white;
   top: 50%;
+  line-height: 1;
   position: absolute;
+  letter-spacing: 3px;
+  font-size: 3rem;
+  font-weight: 300;
+  text-transform: uppercase;
 `;
-
 
 
 
