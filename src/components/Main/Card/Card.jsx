9b488138c325509f4c7e38cb6ec03a736005b5cd
J-Ciro <37349818+J-Ciro@users.noreply.github.com>
2023-05-04 15:11:34 -0500

    Primera parte: Muestreo de las card usando MAP

--- src/components/Main/Card/Card.jsx
+++ src/components/Main/Card/Card.jsx
@@ -1,8 +1,57 @@
 
-import React from 'react'
-import { StyledContainer } from './StyleCard'
-import { StyledTitleContent } from './StyleCard'
-import earth from '../../../images/desktop/image-deep-earth.jpg';
+import React from 'react';
+import { StyledContainer } from './StyleCard';
+import { StyledTitleContent } from './StyleCard';
+import grid from '../../../images/desktop/image-grid.jpg';
+import arcade from '../../../images/desktop/image-night-arcade.jpg';
+import soccer from '../../../images/desktop/image-soccer-team.jpg';
+ import earth from '../../../images/desktop/image-deep-earth.jpg';
+import above from '../../../images/desktop/image-from-above.jpg';
+import borealis from '../../../images/desktop/image-pocket-borealis.jpg';
+import curiosity from '../../../images/desktop/image-curiosity.jpg';
+import fisheye from '../../../images/desktop/image-fisheye.jpg';
+
+const CardItem = [
+  {
+    name: "DEEP EARTH",
+    routes:`${earth}`
+  },
+
+  {
+    name: "NIGHT ARCADE",
+    routes: `${arcade}`
+  },
+  
+  {
+    name: "SOCCER TEAM VR",
+    routes: `${soccer}`
+  },
+
+  {
+    name: "THE GRID",
+    routes: `${grid}`
+  },
+ 
+  {
+    name: "FROM UP ABOVE VR",
+    routes: `${above}`
+  },
+
+  {
+    name: "POCKET BOREALIS",
+    routes: `${borealis}`
+  },
+
+  {
+    name: "THE CURIOSITY",
+    routes: `${curiosity}`
+  },
+
+  {
+    name: "MAKE IT FISHEYE",
+    routes: `${fisheye}`
+  }
+]
 
 
 export const Card = () => {
@@ -15,8 +64,21 @@ export const Card = () => {
         </StyledTitleContent>
         <div>
             <figure>
-                <img src={earth} alt="" />
+            
+        {
+          CardItem.map((item, index)=> <img src={`${item.routes}`} alt={`${item.name}`} key={`${item.name}-${index}`}> 
+            
+          </img>)
+        }
+          {
+          CardItem.map((item, index)=> <span key={`${item.name}-${index}`}> 
+           { item.name}
+          </span>)
+        }
+               
             </figure>
+
+
         </div>
       </StyledContainer>  
     </>
