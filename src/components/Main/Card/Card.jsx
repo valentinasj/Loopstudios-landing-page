a4a6ac99b24987e12b0acc9ce73c463ddc2d362b
angie moreno <guaiagaleria@gmail.com>
2023-05-07 12:30:22 -0500

    adding responsive designe to cards

--- src/components/Main/Card/Card.jsx
+++ src/components/Main/Card/Card.jsx
@@ -1,5 +1,6 @@
 
 import React from 'react';
+import { useState, useEffect } from 'react';
 import { StyledTitleContent, StyledContainerCard,  StyledContainer } from './StyleCard';
 import {CardItem} from './CardItem/CardItem';
 import grid from '../../../images/desktop/image-grid.jpg';
@@ -10,11 +11,61 @@ import above from '../../../images/desktop/image-from-above.jpg';
 import borealis from '../../../images/desktop/image-pocket-borealis.jpg';
 import curiosity from '../../../images/desktop/image-curiosity.jpg';
 import fisheye from '../../../images/desktop/image-fisheye.jpg';
+import grid2 from '../../../images/mobile/image-grid.jpg'
+import arcade2 from '../../../images/mobile/image-night-arcade.jpg'
+import soccer2 from '../../../images/mobile/image-soccer-team.jpg'
+import earth2 from '../../../images/mobile/image-deep-earth.jpg'
+import above2 from '../../../images/mobile/image-from-above.jpg'
+import borealis2 from '../../../images/mobile/image-curiosity.jpg'
+import curiosity2 from '../../../images/mobile/image-curiosity.jpg'
+import fisheye2 from '../../../images/mobile/image-fisheye.jpg'
+
+const listMobile=[
+  {
+    name: "DEEP EARTH",
+    routes:`${earth2}`
+    
+  },
+
+  {
+    name: "NIGHT ARCADE",
+    routes: `${arcade2}`
+  },
+  
+  {
+    name: "SOCCER TEAM VR",
+    routes: `${soccer2}`
+  },
+
+  {
+    name: "THE GRID",
+    routes: `${grid2}`
+  },
+ 
+  {
+    name: "FROM UP ABOVE VR",
+    routes: `${above2}`
+  },
+
+  {
+    name: "POCKET BOREALIS",
+    routes: `${borealis2}`
+  },
+
+  {
+    name: "THE CURIOSITY",
+    routes: `${curiosity2}`
+  },
+
+  {
+    name: "MAKE IT FISHEYE",
+    routes: `${fisheye2}`
+  }
+  
+]
 
 
-
-
-const cardList = [
+const listDesktop = [
   {
     name: "DEEP EARTH",
     routes:`${earth}`
@@ -56,21 +107,36 @@ const cardList = [
     routes: `${fisheye}`
   }
 ]
-
 export const Card = () => {
+  const [cardList, setCardList] = useState([]);
+  useEffect(() => {
+    const updateCardList = () => {
+      const screenWidth = window.innerWidth;
+      if (screenWidth <= 375) {
+        setCardList(listMobile);
+      } else {
+        setCardList(listDesktop);
+      }
+    };
+    updateCardList();
+    window.addEventListener('resize', updateCardList);
+    return () => {
+      window.removeEventListener('resize', updateCardList);
+    };
+  }, []);
+  
+
   return (
     <>
       <StyledContainer>
         <StyledTitleContent>
-            <h6>OUR CREATIONS</h6>
-            <button>SEE ALL</button>
+          <h6>OUR CREATIONS</h6>
+          <button>SEE ALL</button>
         </StyledTitleContent>
         <StyledContainerCard>           
-          {
-            cardList.map((item, index)=> <CardItem imagen={item.routes} texto={item.name} key={index}/> )
-          }
+          {cardList.map((item, index)=> <CardItem imagen={item.routes} texto={item.name} key={index}/> )}
         </StyledContainerCard>
       </StyledContainer>  
     </>
   )
-}
+};
